de2fd5f4dc6a105425aad25aa77e6cdaab31a1be
Deepak Rawat <rahul.aggarwal.ait09@gmail.com>
2020-05-24 19:52:07 +0530

    PythonWeek3Day2

---
+++ PyPy/matplotlib.py
@@ -0,0 +1,11 @@
+class Student:
+ 
+    def __init__(self, nm, ae):
+        self.name=nm
+        self.age=ae
+    #object instance regular
+    def displayStudent(self,ae):
+      print( "Name : ", ae )
+
+rohit=Student(fjfj,6666)
+   
---
+++ PyPy/numpy scipy.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 24 16:03:46 2020
+
+@author: rahul
+"""
+
